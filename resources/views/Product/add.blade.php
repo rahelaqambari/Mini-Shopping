878c3d9831d0ad7d78983773b5c1f52dc00d091d
rahelaqambari <rahelaqambari@gmail.com>
2026-01-22 19:56:09 +0430

    changes

--- resources/views/Product/add.blade.php
+++ resources/views/Product/add.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 
-<div class="w-9/12 mx-auto my-2 border-2 border-black  ">
+<div class="w-full mx-auto flex items-center justify-center flex-col">
     @if ($errors->count()>0)
     
     
@@ -20,15 +20,16 @@
 
 @endforeach
 @endif
-<h1 class="text-center mt-3 text-4xl bg-blue-400 text-white">Add A Product</h1>
-<form method="post" action="{{ URL("product/home") }}" class="flex gap-4 w-full p-4 flex-col" enctype="multipart/form-data">
+<h1 class="text-center mt-3 text-4xl text-stone-400 font-semibold">Add A Product</h1>
+<form method="post" action="{{ URL("product/home") }}" class="flex gap-4  p-4 flex-col w-8/12" enctype="multipart/form-data">
     @csrf
-    <input type="text" name="name" placeholder="Name" class="w-full outline-none border focus:border-0 focus:ring-1 ring-stone-400 py-2 px-4">
-    <input type="number" name="price" placeholder="Price" class="w-full outline-none border focus:border-0 focus:ring-1 ring-stone-400 py-2 px-4">
-    <input type="number" name="quntity" placeholder="Quntity" class="w-full outline-none border focus:border-0 focus:ring-1 ring-stone-400 py-2 px-4">
-    <input type="text" name="madein" placeholder="Made In" class="w-full outline-none border focus:border-0 focus:ring-1 ring-stone-400 py-2 px-4">
-    <input type="file" name="photo" accept="image/*" placeholder="Product Image" class="w-full outline-none border focus:border-0 focus:ring-1 ring-stone-400 py-2 px-4">
-    <textarea name="" class="w-full outline-none border focus:border-0 focus:ring-1 ring-stone-400 py-2 px-4" id=""></textarea>
+    <input type="text" name="name" placeholder="Name" class="w-full outline-none border focus:border-0 rounded-sm focus:ring-1 ring-stone-400 py-2 px-4">
+    <input type="number" name="price" placeholder="Price" class="w-full outline-none border focus:border-0 rounded-sm focus:ring-1 ring-stone-400 py-2 px-4">
+    <input type="number" name="quntity" placeholder="Quntity" class="w-full outline-none border focus:border-0 rounded-sm focus:ring-1 ring-stone-400 py-2 px-4">
+    <input type="text" name="madein" placeholder="Made In" class="w-full outline-none border focus:border-0 rounded-sm focus:ring-1 ring-stone-400 py-2 px-4">
+    <input type="file" name="photo" accept="image/*" placeholder="Product Image" class="w-full outline-none rounded-sm border focus:border-0 focus:ring-1 ring-stone-400 py-2 px-4">
+    <textarea name="p" class="w-full outline-none border focus:border-0 focus:ring-1 ring-stone-400 py-2 px-4 rounded-sm h-24" id="" placeholder="Descraption"></textarea>
+    <button type="submit" class="bg-stone-400 py-2 px-4 w-fit text-white mx-auto rounded-md text-base font-medium">Save </button>
 </form>
 </div>
 </body>
