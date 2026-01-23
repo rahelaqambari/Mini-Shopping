916ca09f4b116869cd64e60e5e6b8fdddeb97599
rahelaqambari <rahelaqambari@gmail.com>
2026-01-23 21:47:54 +0430

    changes

--- resources/views/components/homecoponent.blade.php
+++ resources/views/components/homecoponent.blade.php
@@ -1,3 +1,4 @@
 <div class="w-full flex items-center justify-start bg-[url(157073039.jpg)]">
-   <h1>Online Shopping</h1>
+   <h1 >Online Shopping</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur iure beatae atque? Perferendis voluptate quae animi ullam eaque! Eligendi accusamus provident amet sed! Consequatur inventore dolores assumenda laudantium voluptas consequuntur!</p>
 </div>
