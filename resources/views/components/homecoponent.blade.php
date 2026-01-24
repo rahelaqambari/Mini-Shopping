6031d31727ca1cf3807250a037b1562bff594734
rahelaqambari <rahelaqambari@gmail.com>
2026-01-24 20:06:14 +0430

    changes

--- resources/views/components/homecoponent.blade.php
+++ resources/views/components/homecoponent.blade.php
@@ -7,8 +7,8 @@
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body>
-   <div class="w-full flex items-center justify-start bg-red-500">
-      <img class="w-32 relative " src="img.jpg" alt="">
+   <div class="w-full flex items-center justify-start">
+      <img class=" relative " src="img.jpg" alt="">
    <h1 class="absolute">Online Shopping</h1>
    <p  class="absolute">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur iure beatae atque? Perferendis voluptate quae animi ullam eaque! Eligendi accusamus provident amet sed! Consequatur inventore dolores assumenda laudantium voluptas consequuntur!</p>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        @vite(['resources/css/app.css','resources/js/app.js'])
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -20,9 +21,10 @@
         {{-- @endif --}}
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
+        <header class="flex items-center justify-between w-[105%] -mt-14 bg-zinc-500 text-white py-8 px-3">
+            <h1 class="text-3xl font-serif">ZARYAB Online Shopping</h1>
             @if (Route::has('login'))
-                <nav class="flex items-center justify-end gap-4">
+                <nav class="flex items-center justify-end gap-4 text-white">
                     @auth
                         <a
                             href="{{ url('/dashboard') }}"
@@ -33,7 +35,7 @@
                     @else
                         <a
                             href="{{ route('login') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
+                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-white text-2xl font-serif border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm leading-normal"
                         >
                             Log in
                         </a>
@@ -41,7 +43,7 @@
                         @if (Route::has('register'))
                             <a
                                 href="{{ route('register') }}"
-                                class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                                class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] hover:border-[#1915014a] hover:border text-white text-2xl font-serif dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm leading-normal">
                                 Register
                             </a>
                         @endif
