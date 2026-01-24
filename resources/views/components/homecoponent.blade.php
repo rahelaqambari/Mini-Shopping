08552f61492ef23e87dab47526c55edc0bd5ad3b
rahelaqambari <rahelaqambari@gmail.com>
2026-01-24 19:46:51 +0430

    changes

--- resources/views/components/homecoponent.blade.php
+++ resources/views/components/homecoponent.blade.php
@@ -1,4 +1,16 @@
-<div class="w-full flex items-center justify-start bg-[url(157073039.jpg)]">
-   <h1 >Online Shopping</h1>
-   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur iure beatae atque? Perferendis voluptate quae animi ullam eaque! Eligendi accusamus provident amet sed! Consequatur inventore dolores assumenda laudantium voluptas consequuntur!</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+<body>
+   <div class="w-full flex items-center justify-start bg-red-500">
+      <img class="w-32 relative " src="img.jpg" alt="">
+   <h1 class="absolute">Online Shopping</h1>
+   <p  class="absolute">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur iure beatae atque? Perferendis voluptate quae animi ullam eaque! Eligendi accusamus provident amet sed! Consequatur inventore dolores assumenda laudantium voluptas consequuntur!</p>
 </div>
+</body>
+</html>
